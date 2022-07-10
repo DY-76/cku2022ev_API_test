c0a8ce6e0b1254142aa97f3922707a2e1495966b
DY-76 <9754745@naver.com>
2022-07-10 16:07:30 +0900

    SecretKeys git page bug fix

--- SecretKeys.js
+++ SecretKeys.js
@@ -1,3 +1,7 @@
 export const serviceKey = () => {
     return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
 }
+
+function serviceKey() {
+    return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
+}
