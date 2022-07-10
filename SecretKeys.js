674d54ee167874920294fe9701e93e77e18aacfe
DY-76 <9754745@naver.com>
2022-07-10 20:26:58 +0900

    SecretKeys git page bug fix

--- SecretKeys.js
+++ SecretKeys.js
@@ -1,9 +1,16 @@
 // 모듈
-// export const serviceKey = () => {
+export function Keys(name){
+    switch (name){
+        case "serviceKey":
+            return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
+
+        case "kakaoApiKey":
+             return "e22cdd82c10efbbd5945b45ce7749553";
+
+    }
+    alert("!case 없음!");
+}
+// 함수
+// function serviceKey() {
 //     return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
 // }
-
-// 함수
-function serviceKey() {
-    return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
-}
