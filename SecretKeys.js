a99bb2ecc10c6976d342bbf08ba6ce3a1bb3dac3
DY-76 <9754745@naver.com>
2022-07-10 16:12:38 +0900

    SecretKeys git page bug fix

--- SecretKeys.js
+++ SecretKeys.js
@@ -1,7 +1,9 @@
-export const serviceKey = () => {
-    return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
-}
+// 모듈
+// export const serviceKey = () => {
+//     return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
+// }
 
+// 함수
 function serviceKey() {
     return "Zxc%2BL1BY7vTH4mkcjzGShFsue5yUAk2q55yjb3nUf7EeeXcsQTv9nE7qIjVN2oU01PuQMJ%2BiHQGuo2fa2ZlJlw%3D%3D";
 }
