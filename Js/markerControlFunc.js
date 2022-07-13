07f1e5a33fe7278db5f198df0d31504e7cb41a00
DY-76 <9754745@naver.com>
2022-07-13 16:56:34 +0900

    list marker Bug fix

--- Js/markerControlFunc.js
+++ Js/markerControlFunc.js
@@ -95,6 +95,8 @@ function hideMarkers() {
 
 //리스트에서 클릭했을때, 마커 추가용
 function listClick(index) {
+    console.log(GroupDatas)
+    console.log(GroupDatas[index])
     addMarker(new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi), Names)
 }
 
