c91041dfc098f2ba6eae618fac571ebc1f98eee6
DY-76 <9754745@naver.com>
2022-07-13 20:10:21 +0900

    map bug fix

--- Js/markerControlFunc.js
+++ Js/markerControlFunc.js
@@ -104,7 +104,7 @@ function moveMap(position) {
 
 //리스트에서 클릭했을때, 마커 추가용
 function listClick(index) {
-    addMarker(new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi), [GroupDatas[index][0].csNm]);
+    addMarker(new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi), [GroupDatas[index-1][0].csNm]);
     popupOn();
     map.relayout();
     moveMap(new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi));
