8065a9a447814648d57022931312a5201ce5ea14
DY-76 <9754745@naver.com>
2022-07-13 19:33:20 +0900

    map center load update

--- Js/markerControlFunc.js
+++ Js/markerControlFunc.js
@@ -97,6 +97,11 @@ function hideMarkers() {
 function listClick(index) {
     console.log(GroupDatas,index)
     console.log(GroupDatas[0])
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi), //지도의 중심좌표.
+    level: 8 //지도의 레벨(확대, 축소 정도)
+    };
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
     addMarker(new kakao.maps.LatLng(GroupDatas[index][0].lat, GroupDatas[index][0].longi), Names[index])
 }
 
